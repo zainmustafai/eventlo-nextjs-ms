ec3f4b41d29bd1b95042301f0292fc44f5bb4699
zainmustafai <zforzain2000@gmail.com>
2024-01-15 03:28:34 +0500

    'database'

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-const MONGODB_URI = process.env.NEXT_MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || {
     conn: null,
