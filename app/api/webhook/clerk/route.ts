818e5f91e4c49e5ab64ba4b5b2826788f79d1bbc
zainmustafai <zforzain2000@gmail.com>
2024-01-16 00:45:14 +0500

    asd

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -55,7 +55,6 @@ export async function POST(req: Request) {
     const eventType = evt.type;
 
     if (eventType === 'user.created') {
-        alert('Hello');
         const { id, email_addresses, image_url, first_name, last_name, username, } = evt.data;
         const user = {
             clerkId: id,
